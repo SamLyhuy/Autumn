539030a72ee183e5ba395a0ef480a7530be303ab
LyHuy <lyhuy011@gmail.com>
2025-01-07 03:03:10 +0700

    start create signUp function

---
+++ app/src/main/java/kh/edu/rupp/ite/autumn/data/model/RegisterData.kt
@@ -0,0 +1,9 @@
+package kh.edu.rupp.ite.autumn.data.model
+
+data class RegisterData(
+    val name: String,
+    val email: String,
+    val phonenumber: String,
+    val password: String,
+    val profile: String,
+)
